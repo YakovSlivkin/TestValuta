b820c3d211f44aeb7a3a7094165ee83ab0e05a15
YakovSlivkin <39372968+YakovSlivkin@users.noreply.github.com>
2018-05-18 01:01:25 +0300

    Add files via upload

---
+++ TestValuta/TestValuta/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Write your JavaScript code.
+function Load() {
+    $.get("/Home/Valuta", null, function (data) {
+        valuta = $.parseJSON(data)
+        values = valuta.stock;
+        var line = "";
+        for (var i = 0; i < values.length; i++)
+            line = line + "<tr><td height=50>" + values[i].name + "</td>" + "<td align=right>" + values[i].volume + "</td>" + "<td align=right>" + values[i].price.amount.toFixed(2) + "</td></tr>";
+        $(".rows").html(line);
+    });
+};
+
+function Start() {
+    Load();
+    setTimeout(Load, 15000);
+}
+
